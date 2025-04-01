403e567aee24c2d677a0ca63c3f96e321de3e531
Cheese <164827551+terutora@users.noreply.github.com>
2025-04-02 02:26:02 +0900

    環境変数をセットしていないため

--- src/app/api/webhook/clerk/route.js
+++ src/app/api/webhook/clerk/route.js
@@ -1,3 +1,4 @@
+/*{
 // src/app/api/webhook/clerk/route.js
 import { Webhook } from "svix";
 import { headers } from "next/headers";
@@ -80,3 +81,4 @@ export async function POST(req) {
 
   return NextResponse.json({ message: `Webhook received: ${type}` });
 }
+}*/
